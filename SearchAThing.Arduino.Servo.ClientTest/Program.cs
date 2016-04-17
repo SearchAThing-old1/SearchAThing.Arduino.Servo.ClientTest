b0d58e83765ea839e372a8ac832958bc4c3f2cb7
Lorenzo Delana <lorenzo.delana@gmail.com>
2016-04-17 22:29:30 +0200

    initial import.

---
+++ SearchAThing.Arduino.Servo.ClientTest/Program.cs
@@ -0,0 +1,45 @@
+﻿#region SearchAThing.Arduino, Copyright(C) 2016 Lorenzo Delana, License under MIT
+/*
+* The MIT License(MIT)
+* Copyright(c) 2016 Lorenzo Delana, https://searchathing.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+#endregion
+
+using SearchAThing.Net.SRUDP.TCPBridge.ServoClient;
+using System;
+
+namespace SearchAThing.Arduino.Servo.ClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ServoClient("192.168.0.80", 51000, "192.168.0.40", 50000);
+
+            while (true)
+            {
+                var a = client.AnalogRead(0);
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"a={a} v={a / 1023.0 * 4.52}");
+            }
+        }
+    }
+}
